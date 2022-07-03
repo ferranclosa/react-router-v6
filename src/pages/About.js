0cda20988a63d5fb52f097556d8e83e39be734cd
Ferran Closa Cervera <36318284+ferranclosa@users.noreply.github.com>
2022-07-03 02:48:09 +0200

    some extra explanations

--- src/pages/About.js
+++ src/pages/About.js
@@ -22,20 +22,29 @@ import { useNavigate, useLocation } from "react-router-dom"
             let y = new Date(location.state.someState.today)
             let x = someState.one + someState.two + someState.twenty
             let h = new Date(y.getFullYear() + x, y.getMonth(), y.getDate())
-            setNewDate( h.toLocaleString() )
+            setNewDate( h.toLocaleDateString() )
 
         }, [])
         
         return (
             <>
                 <h1>About</h1>
+                <p>On this page we are seeing the parameters (state) passed 
+                    to the page. We note some deconstructing. 
+                    Also we apply a simple formula where we add the 
+                    items from the state (one, two and twenty) and the 
+                    result gets added in years to the original date 
+                    and displayed on the new Date field  (notice that 
+                    23 years have been added )
+                </p>
+                <p>This is an oversimplified example but it helps to grasp the concept</p>
                 <code> The state is {JSON.stringify(state)}</code>
                 <br/>
-                <code> someObject is {JSON.stringify(someState)} </code>
+                <code> someObject is = {JSON.stringify(someState)} </code>
                 <br></br>
-                <code>New Date is {newDate}</code>
+                <code>New Date is = {newDate}</code>
                 <br/>
-                <code>Location is {JSON.stringify(location)}</code>
+                <code>The full Location is = {JSON.stringify(location)}</code>
 
                 <br/>
                 <button onClick={handleBack} >Back</button>
